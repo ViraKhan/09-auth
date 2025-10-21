a1a8f9db687bf43f6284dff2ca96176a63497de1
Vira Khan <vira.khan1@gmail.com>
2025-10-21 11:22:14 +0200

    Fix fetchNoteById export

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
-import { fetchNoteById } from "@/lib/api/api";
+import { fetchNoteById } from "@/lib/api/clientApi";
 import type { Metadata } from "next";
 
 interface NoteDetailsProps {
