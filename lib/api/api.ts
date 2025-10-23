87abf099a99a10f46fb8d12c32507ffd3db1f8fd
Vira Khan <vira.khan1@gmail.com>
2025-10-23 15:07:08 +0200

    update url

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL ?
- `${process.env.NEXT_PUBLIC_API_URL}/api`
-    : "https://notehub-api.goit.study/api";
+const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
 const apiClient = axios.create({
   baseURL,
